7adcba6f4488240430ca1e1cc959b5b6b9aac15f
gabebarros <gabe.barros@icloud.com>
2025-04-19 21:49:28 -0700

    Added play stack, no scoring logic yet

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -7,6 +7,7 @@ public class Controller {
     private Game game;
     private View view;
     private int cribCount = 0;
+    private boolean isPlayer1Turn = true;
 
     public Controller(Game game, View view) {
         this.game = game;
@@ -19,13 +20,13 @@ public class Controller {
         return game;
     }
 
-    public void showHands() {
+    public void playCrib() {
         view.updatePlayerHand(game.getPlayer1(), view.getPlayer1Panel(), e -> {
             int index = Integer.parseInt(e.getActionCommand());
             if (cribCount < 2) {
                 game.addToCrib(game.getPlayer1().playCard(index));
                 cribCount++;
-                showHands();
+                playCrib();
             }
         });
 
@@ -34,13 +35,47 @@ public class Controller {
             if (cribCount >= 2 && cribCount < 4) {
                 game.addToCrib(game.getPlayer2().playCard(index));
                 cribCount++;
-                showHands();
+                playCrib();
 
                 if (cribCount == 4) {
                     game.drawStarterCard(); // trigger observer to update starter card
+                    playCardToStack(); // begin card play phase
                 }
             }
         });
+    }
+    
+    public void playCardToStack() {
+        if (isPlayer1Turn) {
+            view.updatePlayerHand(game.getPlayer1(), view.getPlayer1Panel(), e -> {
+                int index = Integer.parseInt(e.getActionCommand());
+                Card playedCard = game.getPlayer1().playCard(index);
+                game.playCard(playedCard, game.getPlayer1());
+                isPlayer1Turn = false;
+                updateHands();  // Just update display instead of recursive call
+            });
+
+            view.updatePlayerHand(game.getPlayer2(), view.getPlayer2Panel(), e -> {}); // Disable clicks
+        } else {
+            view.updatePlayerHand(game.getPlayer2(), view.getPlayer2Panel(), e -> {
+                int index = Integer.parseInt(e.getActionCommand());
+                Card playedCard = game.getPlayer2().playCard(index);
+                game.playCard(playedCard, game.getPlayer2());
+                isPlayer1Turn = true;
+                updateHands();
+            });
+
+            view.updatePlayerHand(game.getPlayer1(), view.getPlayer1Panel(), e -> {}); // Disable clicks
+        }
+    }
+
+    private void updateHands() {
+        // Just refreshes the view to show updated hands after a play
+        view.updatePlayerHand(game.getPlayer1(), view.getPlayer1Panel(), e -> {});
+        view.updatePlayerHand(game.getPlayer2(), view.getPlayer2Panel(), e -> {});
+
+        // Now assign listeners again to allow the current player to click
+        playCardToStack();  // Safe, as it doesn't recurse into itself after click
     }
     
     
--- src/main/model/GameObserver.java
+++ src/main/model/GameObserver.java
@@ -5,4 +5,6 @@ import java.util.ArrayList;
 public interface GameObserver {
     void onCribUpdated();
     void onStarterCardDrawn(Card card);
+    void onPlayStackUpdated(ArrayList<Card> playStack);
+    void onScoreUpdated(int player1Score, int player2Score);
 }
