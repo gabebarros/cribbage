c90f77f083fe77d5438459ade36b49beaa90f51c
ankit <ankit195300@gmail.com>
2025-04-24 23:55:43 -0700

    Completed writing tests for coverage needs. Fixed some escaping references.

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -8,11 +8,11 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 import main.model.Card;
-import main.model.Deck;
 import main.model.Game;
 import main.model.Rank;
 import main.model.Suit;
-import main.model.Scorer;
+import main.model.GameObserver;
+
 
 public class GameTest {
 	@Test
@@ -248,6 +248,69 @@ public class GameTest {
 		
 		assertEquals(g1.getPlayer1Wins(), 3);
 		assertEquals(g1.getPlayer2Wins(), 1);
+	}
+	
+	@Test
+	public void testOriginalHands() {
+		Game g1 = new Game("a", "b");
+		
+		ArrayList<Card> h1 = new ArrayList<Card>();
+		ArrayList<Card> h2 = new ArrayList<Card>();
+		
+		h1.add(Card.getCard(Rank.ACE, Suit.CLUBS));
+		h1.add(Card.getCard(Rank.ACE, Suit.SPADES));
+		
+		h2.add(Card.getCard(Rank.ACE, Suit.HEARTS));
+		h2.add(Card.getCard(Rank.ACE, Suit.DIAMONDS));
+		
+		g1.setPlayer1OriginalHand(h1);
+		g1.getPlayer1().setHand(h1);
+		
+		g1.setPlayer2OriginalHand(h2);
+		g1.getPlayer2().setHand(h2);
+		
+		ArrayList<Card> ps = new ArrayList<Card>();
+		
+		ps.add(Card.getCard(Rank.JACK, Suit.HEARTS));
+		ps.add(Card.getCard(Rank.QUEEN, Suit.HEARTS));
+		ps.add(Card.getCard(Rank.KING, Suit.HEARTS));
+		ps.add(Card.getCard(Rank.THREE, Suit.HEARTS));
+		
+		g1.setPlayStack(ps);
+		
+		for(int i = 0; i < 2; i++) {
+			assertEquals(h1.get(i), g1.getPlayer1OriginalHand().get(i));
+			assertEquals(h2.get(i), g1.getPlayer2OriginalHand().get(i));
+		}
+		
+	}
+	
+	public class TestObserver implements GameObserver {
+		public TestObserver() {
+		}
+		
+	    public void onCribUpdated() {};
+	    public void onStarterCardDrawn(Card card) {};
+	    public void onPlayStackUpdated(ArrayList<Card> playStack) {};
+	    public void onScoreUpdated(int player1Score, int player2Score) {};
+	}
+	
+	@Test
+	public void testControllerFunction() {
+		TestObserver to = new TestObserver();
+		Game g1 = new Game("a", "b");
+		
+		assertEquals(g1.getObserverCount(), 0);
+		
+		g1.addObserver((main.model.GameObserver)to);
+		g1.updateScore();
+		
+		assertEquals(g1.getObserverCount(), 1);
+		
+		g1.drawStarterCard();
+		assertEquals(g1.getObserverCount(), 1);
+		assertTrue(g1.getStartCard()!=null);
+	
 	}
 	
 
